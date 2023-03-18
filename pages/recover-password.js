c571f7fa7ac52b8f48bbf3ef68cc3a7328c8a4ce
nam-td <namtd.developer@gmail.com>
2023-03-18 17:59:38 +0700

    Fix typo in RecoverPasswordForm component's name

--- pages/recover-password.js
+++ pages/recover-password.js
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic"
-const RecoverPasswordForm = dynamic(() => import("components/RecoverPasswordForm"), {
+const RecoverPasswordForm = dynamic(() => import("@/components/RecoverPasswordForm"), {
   ssr: false
 })
 
